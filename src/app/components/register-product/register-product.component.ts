1759223bf90468465e2ac8808feec1737bbeea29
Fernando Cavaccini <fernandocavaccini@gmail.com>
2024-03-19 18:52:17 -0300

    Adicionado validação para formato inválido de imagem

--- src/app/components/register-product/register-product.component.ts
+++ src/app/components/register-product/register-product.component.ts
@@ -3,6 +3,8 @@ import { ProductService } from 'src/app/services/product/product.service';
 import { ProductModel } from 'src/app/models/ProductModel';
 import { NgForm, NgModel } from '@angular/forms';
 import { ProductImageModel } from 'src/app/models/ProductImage';
+import { MatDialog } from '@angular/material/dialog';
+import { ApiResponseDialogComponent } from '../api-response-dialog/api-response-dialog.component';
 
 @Component({
   selector: 'app-register-product',
@@ -13,9 +15,10 @@ export class RegisterProductComponent {
 
   product = {} as ProductModel;
   productImage = {} as ProductImageModel;
+  fileError: string = '';
 
-  constructor(private productService: ProductService){}
-  
+  constructor(private productService: ProductService, private dialog: MatDialog) { }
+
   saveProduct(form: NgForm) {
     this.productService.saveProduct(this.product).subscribe(
       (response: any) => {
@@ -27,6 +30,10 @@ export class RegisterProductComponent {
             alert('Produto salvo com sucesso');
           })
         this.cleanForm(form);
+
+      },
+      (error) => {
+        this.openDialog('Erro ao tentar cadastrar produto' + error);
       }
     )
   }
@@ -34,13 +41,29 @@ export class RegisterProductComponent {
   onFileSelected(event: any) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      this.productImage.file = file;
+      const allowedTypes = ['image/jpg', 'image/png']
+
+      if (file && allowedTypes.includes(file.type)) {
+        this.productImage.file = file;
+        this.fileError = '';
+      } else {
+        this.openDialog('Tipo de arquivo inválido. Apenas imagens JPG, PNG são permitidas.')
+        this.productImage.file = null
+        event.target.value = null;
+      }
     }
   }
 
-  cleanForm(form: NgForm){
+  cleanForm(form: NgForm) {
     form.resetForm();
     this.product = {} as ProductModel;
+  }
+
+  openDialog(apiResponse: string): void {
+    const dialogRef = this.dialog.open(ApiResponseDialogComponent, {
+      width: '300px',
+      data: apiResponse,
+    });
   }
 
   title = 'register-product'
